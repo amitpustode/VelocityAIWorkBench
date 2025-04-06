55aeb6b36391bcd7c9997388ff2d974ed2c99036
amitpustode <arpustode@gmail.com>
2025-04-06 14:05:05 +0530

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     root: __dirname,
-    include: ['test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    // include: ['test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     testTimeout: 1000 * 29,
     coverage: {
       provider: 'v8', // or 'c8'
