df1f96394113415e235c1170b14ba15f20348350
amitpustode <arpustode@gmail.com>
2025-04-08 19:18:11 +0530

    Delete src directory

--- src/utility/logger.ts
+++
@@ -1,6 +0,0 @@
-import winston from "winston";
-
-// Helper function to log events
-export const logEvent = (level: keyof typeof winston.config.npm.levels, message: string) => {
-    window.electronAPI.logEvent(level, message);
-  };
